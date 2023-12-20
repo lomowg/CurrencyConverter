152b15c403a27ec611dd6bd4fe80807c39e2a8d2
Lomowg <sokol789123@yandex.ru>
2023-12-20 12:27:07 +0300

    Added converter.html page, changed index.html main page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,6 +8,18 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/converter', methods=['GET', 'POST'])
+def converter():
+    if request.method == 'POST':
+        # Получаем данные из формы
+        amount = request.form['amount']
+        from_currency = request.form['fromCurrency']
+        to_currency = request.form['toCurrency']
+
+        return f'Перевод {amount} {from_currency} в {to_currency}'
+
+    return render_template('converter.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
