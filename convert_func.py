a2acebc03339831c47fe7d948307923c640188c5
Lomowg <sokol789123@yandex.ru>
2023-12-21 19:05:22 +0300

    Added converter function

---
+++ convert_func.py
@@ -0,0 +1,22 @@
+def convert(input_cur, output_cur, amount, reverse=False):
+    from pycbrf import ExchangeRates
+    from datetime import datetime
+
+    date_now = datetime.now().date().isoformat()
+    rates = ExchangeRates(date_now, locale_en=True)
+
+    if input_cur == output_cur:
+        multiplier_1 = rates[input_cur].rate / rates[output_cur].rate
+        multiplier_2 = 1 / multiplier_1
+    elif input_cur == 'RUB':
+        multiplier_1 = rates[output_cur].rate
+        multiplier_2 = 1/rates[output_cur].rate
+    elif output_cur == 'RUB':
+        multiplier_1 = 1/rates[input_cur].rate
+        multiplier_2 = rates[input_cur].rate
+    else:
+        multiplier_1 = rates[input_cur].rate/rates[output_cur].rate
+        multiplier_2 = 1/multiplier_1
+
+    return amount*float(multiplier_2) if reverse else amount*float(multiplier_1)
+
