c0c7181a169686a27044d1360d4c7a9ccaadb21b
Lomowg <sokol789123@yandex.ru>
2023-12-21 21:53:04 +0300

    A working conversion is made

--- convert_func.py
+++ convert_func.py
@@ -5,6 +5,9 @@ def convert(input_cur, output_cur, amount, reverse=False):
     date_now = datetime.now().date().isoformat()
     rates = ExchangeRates(date_now, locale_en=True)
 
+    if isinstance(amount, str) or amount is None:
+        return 0
+
     if input_cur == output_cur:
         multiplier_1 = rates[input_cur].rate / rates[output_cur].rate
         multiplier_2 = 1 / multiplier_1
@@ -19,4 +22,3 @@ def convert(input_cur, output_cur, amount, reverse=False):
         multiplier_2 = 1/multiplier_1
 
     return amount*float(multiplier_2) if reverse else amount*float(multiplier_1)
-
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from convert_func import convert
 
 app = Flask(__name__)
 
@@ -10,15 +11,26 @@ def index():
 
 @app.route('/converter', methods=['GET', 'POST'])
 def converter():
-    if request.method == 'POST':
-        # Получаем данные из формы
-        amount = request.form['amount']
-        from_currency = request.form['fromCurrency']
-        to_currency = request.form['toCurrency']
+    return render_template('converter.html', convert=convert)
 
-        return f'Перевод {amount} {from_currency} в {to_currency}'
+@app.route('/convert1', methods=['POST'])
+def convert1():
+    input_currency = request.json['inputCurrency'].upper()
+    output_currency = request.json['outputCurrency'].upper()
+    input_value = request.json['inputValue']
+    result = convert(input_currency, output_currency, input_value)
 
-    return render_template('converter.html')
+    return jsonify(result=result)
+
+
+@app.route('/convert2', methods=['POST'])
+def convert2():
+    input_currency = request.json['inputCurrency'].upper()
+    output_currency = request.json['outputCurrency'].upper()
+    input_value = request.json['inputValue']
+    result = convert(input_currency, output_currency, input_value, reverse=True)
+
+    return jsonify(result=result)
 
 
 if __name__ == '__main__':
